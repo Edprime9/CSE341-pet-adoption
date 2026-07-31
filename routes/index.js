d711e562b5e00c56bc8b1bbeb43a971795395bf1
Dianauguerra <diana.1609@hotmail.com>
2026-07-31 14:16:59 -0600

    single quote

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const petsRouter = require('./pets');
 const applicationsRouter = require('./applications');
-const sheltersRouter = require("./shelters");
+const sheltersRouter = require('./shelters');
 
 router.use('/', petsRouter);
 router.use('/', applicationsRouter);
